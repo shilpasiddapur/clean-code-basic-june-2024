f8830c2c5ebd035f642959efb0e95a4691b7448f
shilpasiddapur <43199526+shilpasiddapur@users.noreply.github.com>
2024-06-25 15:37:02 +0530

    Create Abstraction.cpp

---
+++ Abstraction.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+class IPrintableDevice{
+    public :
+    virtual void print() = 0;
+};
+
+class IScanableDevice{
+    public :
+    virtual void scan() = 0;
+};
+class D 
+{
+    private:
+        int val;
+    protected:
+        D(int x) { val = x; std::cout << "D Constructor, val = " << val << std::endl;}
+    public:
+        ~D() { std::cout << "D destructor called " << std::endl;}
+        void printVal() { std::cout << "Val in D = " << val << std::endl;}
+    
+};
+ 
+class P : public D, public IPrintableDevice
+{
+    public:
+        P() : D(1) { std::cout << "P Constructor" << std::endl;}
+        ~P() { std::cout << "P destructor called " << std::endl;}
+        void print() { std::cout << "Printing..." << std::endl;}
+};
+ 
+class S : public D, public IScanableDevice
+{
+    public:
+        S() : D(2)
+        { std::cout << "S Constructor" << std::endl;}
+        ~S() { std::cout << "S destructor called " << std::endl;}
+        void scan() { std::cout << "Scanning..." << std::endl;}
+};
+ 
+class PS : public D,public IScanableDevice, public IPrintableDevice
+{
+    P pObj;
+    S sObj;
+    public:
+        PS(int x) : D(x) { std::cout << "PS Constructor" << std::endl;}
+        ~PS() { std::cout << "PS destructor called " << std::endl;}
+        void print(){
+            pObj.print();
+        }
+        
+        void scan(){
+            sObj.scan();
+        }
+};
+ 
+void printTask(IPrintableDevice* pptr)
+{
+    pptr->print();
+}
+ 
+void scanTask(IScanableDevice* sptr)
+{
+    sptr->scan();
+}
+ 
+int main()
+{
+    P pObj;
+    S sObj;
+    PS psObj{10};
+ 
+    printTask(&pObj);
+    printTask(&psObj);
+ 
+    scanTask(&sObj);
+    scanTask(&psObj);
+    
+    return 0;
+}
